7c795f23a5c04e9c358c06523414c0d571e39359
tejesh <teju835374@gmail.com>
2023-03-09 00:17:52 +0530

    fix env

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-INSTAGRAM_USERNAME= os.environ('INSTA_USERNAME')
-INSTAGRAM_PASSWSORD=os.environ('INSTA_PASSWORD')
+INSTAGRAM_USERNAME= os.environ["INSTA_USERNAME"]
+INSTAGRAM_PASSWSORD=os.environ["INSTA_PASSWORD"]
 DATABASE="sqlite:///instagram.db"
